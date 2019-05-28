0e01fc679b950200159f3548e339b93c53f4a6af
gtgeorgiev <gtggeorgiev@abv.bg>
2019-05-28 17:36:48 +0300

    Sample edit in routes.js

--- routes.js
+++ routes.js
@@ -4,6 +4,7 @@ const fortunes = [
   "Conquer your fears or they will conquer you.",
   "Rivers need springs.",
   "Do not fear what you don't know.",
+  "Whenever possible, keep it simple.",
   "You will have a pleasant surprise.",
   "Whenever possible, keep it simple.",
 ];
